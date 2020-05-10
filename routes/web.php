64f48f9bae9c7790ab69ee707cccc07101dd4be1
Takuya Asano <yokinum@gmail.com>
2020-05-10 12:21:31 +0900

    PHP/Laravel 09 課題です

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
+});
+
+//  課題３
+Route::get('XXX', 'AAAController@bbb'); 
